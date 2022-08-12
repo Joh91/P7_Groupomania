e10beb19c8a327683862ce420d77ab31f5d5d8d4
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-08-12 17:18:22 +0200

    new models

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,13 +2,25 @@
 const mongoose = require("mongoose"); 
 //importation de unique validator pour l'utilisation d'un mail unique
 const uniqueValidator = require("mongoose-unique-validator"); 
+// importation de validator 
+const { isEmail } = require('validator')
 
 //définition des modèles Users afin d'enregistrer les données dans la database
 const userSchema = mongoose.Schema({
-    pseudo: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true },
-}); 
+        pseudo: {type: String, required: true, minLength: 3, maxLength: 25, unique: true, trimp: true},
+        email: {type: String, required: true, validator: [isEmail], unique: true, lowercase: true},
+        password: {type: String, required: true },
+        imageUrl: {type: String},
+        bio: {type: String, max: 1000},
+        followers: {type: [String]},
+        following: {type: [String]}, 
+        like: {type: number}, 
+        userLike: {type: [String]},
+    }, 
+    {
+        timestamps: true, 
+    }
+); 
 
 userSchema.plugin(uniqueValidator)
 

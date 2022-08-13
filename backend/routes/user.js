f63c4179e75c53d60ad948f13cca5613e94b50f2
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-08-13 15:54:19 +0200

    get userProfil config

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,14 @@
 // importations 
 const router = require('express').Router(); 
 const userCtrl = require('../controllers/user')
+const userProfilCtrl = require('../controllers/user.profil')
 
 
 // routes 
 router.post('/signup', userCtrl.signup)
 // router.post('/login',  userCtrl.login);
 
+router.get('/', userProfilCtrl.getAllUsers); 
+router.get('/:id', userProfilCtrl.getUserInfo)
 
 module.exports = router; 

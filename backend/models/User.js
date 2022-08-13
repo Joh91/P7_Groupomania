f8418a5c714467750d9fc4f9815f13d9bf1d55d1
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-08-13 15:53:07 +0200

    email validator replaced by regex

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,13 +2,11 @@
 const mongoose = require("mongoose"); 
 //importation de unique validator pour l'utilisation d'un mail unique
 const uniqueValidator = require("mongoose-unique-validator"); 
-// importation de validator 
-const { isEmail } = require('validator')
 
 //définition des modèles Users afin d'enregistrer les données dans la database
 const userSchema = mongoose.Schema({
         pseudo: {type: String, required: true, minLength: 3, maxLength: 25, unique: true, trimp: true},
-        email: {type: String, required: true, validator: [isEmail], unique: true, lowercase: true},
+        email: {type: String, required: true, unique: true, lowercase: true},
         password: {type: String, required: true },
         imageUrl: {type: String},
         bio: {type: String, max: 1000},

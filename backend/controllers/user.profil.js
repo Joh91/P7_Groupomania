1b3ef4fea884c19a5b27dc31b4f9d7652aaad8c2
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-08-14 16:29:26 +0200

    delete & modify profile

--- backend/controllers/user.profil.js
+++ backend/controllers/user.profil.js
@@ -9,10 +9,41 @@ exports.getAllUsers = (req, res, next) => {
     .catch((error) => {res.status(400).json({error})})
 }; 
 
-/*---- requête Get pour afficher un user -----*/ 
+/*---- requête Get pour afficher le profil -----*/ 
 exports.getOneUser = (req, res, next) => {
     //récupération d'un compte à l'aide de l'Id sans afficher le mdp
     User.findOne({ _id: req.params.id}).select('-password')
     .then((user) => {res.status(200).json(user)})
     .catch((error) => {res.status(400).json({error})})
+};
+
+/*---- requête Put pour modifier le profil ----*/
+exports.updateUser = (req, res, next) => {
+    try {
+        User.findOneAndUpdate(
+            // recherche de l'id dans la bdd 
+            {_id: req.params.id}, 
+            // modification de la bio 
+            {$set: {bio : req.body.bio, _id: req.params.id}},
+            (err, data) => {
+                // si aucune erreur les données sont envoyées 
+                if(!err) return res.send(data); 
+                // si une erreur un message d'erreur est retourné
+                if (err) return res.status(500).send({ err });
+            })
+    } catch (err) {
+        return res.status(500).json({ err });
+    } 
+}; 
+
+exports.deleteUser = (req, res, next) => {
+    User.findOne({_id: req.params.id})
+    .then (user => {
+        user.deleteOne({_id: req.params.id})
+        .then(() => {res.status(200).json({message: "profil supprimé !"})})
+        .catch(error => res.status(401).json({ error }));
+    })
+    .catch ((error) => {
+        res.status(500).json({error}); 
+    })
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,6 +9,8 @@ router.post('/signup', userCtrl.signup)
 // router.post('/login',  userCtrl.login);
 
 router.get('/', userProfilCtrl.getAllUsers); 
-router.get('/:id', userProfilCtrl.getOneUser)
+router.get('/:id', userProfilCtrl.getOneUser);
+router.put('/:id', userProfilCtrl.updateUser); 
+router.delete('/:id', userProfilCtrl.deleteUser); 
 
 module.exports = router; 

0fce9d0995531d9922bd06096736c4e41d1f5b5c
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-08-26 21:07:42 +0200

    axios headers config

--- frontend/groupomania-frontend/src/components/PostSection.vue
+++ frontend/groupomania-frontend/src/components/PostSection.vue
@@ -13,21 +13,28 @@
 
             <div class= "post">
                 <div class="head-post">
-                    <h3>Thomas 35</h3>
-                    <h4>Publié il y a 2h</h4>
+                    <div class = "head-post-title">
+                        <h3>Thomas 35</h3>
+                        <h4>Publié il y a 2h</h4>
+                    </div>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-gear"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Modifier</a></li>
+                            <li><a class="dropdown-item" href="#">Supprimer</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="card">
                     <img class= "post-img" src="https://media.istockphoto.com/photos/overhead-view-of-a-chinese-new-year-reunion-dinner-activity-picture-id1288747002?b=1&k=20&m=1288747002&s=170667a&w=0&h=JCmT1TlO_NmTVrSfuGa0_h5IBDQ9KDgmd5559VxyWa8=" alt=""/>
                     <div class="card-body">
                         <p class="card-text">Superbe réunion culinaire avec l'équipe de Shanghai</p>
-                        <ul>
-                            <li>Like</li>
-                            <li>modifier</li>
-                            <li>supprimer</li>
-                        </ul>
-                        <!-- <div class="foot">
-                            <i class="fa-solid fa-thumbs-up"></i>
-                        </div> -->
+                        <div class="foot">
+                            <i class="fa-solid fa-thumbs-up" ></i>
+                            <button @click="getPosts">click</button>
+                        </div>
                     </div>
                 </div>   
             </div> 
@@ -35,9 +42,24 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 export default ({
-    name: "PostSection"
+    name: "PostSection", 
+    data(){
+        return {
+            allPosts: []
+        }
+    }, 
+        created(){
+            const token = localStorage.getItem('token');
+            const headers = {
+                Authorization: 'Bearer ' + token};
+
+            axios.get('http://localhost:3000/api/posts', {headers} )
+            .then((response) => {
+                console.log(response); 
+            })
+        }
 })
 </script>
 
@@ -57,7 +79,7 @@ export default ({
 }
 
 .get-post h3 {
-    font-size: 20px;
+    font-size: 18px;
     font-weight: bold;
 }
 
@@ -98,7 +120,7 @@ export default ({
     gap: 15px; 
     justify-content: space-between;
     margin: 40px 0 0;
-    padding: 10px; 
+    padding: 15px 30px 10px; 
     border-radius: 15px 15px 0 0;
     background-color: #faebeb;
 }
@@ -112,17 +134,24 @@ export default ({
     font-size: 12px; 
 }
 
+.dropdown-toggle {
+    font-size: 12px;
+    background: #4E5166;
+}
+
 .post-img {
     height: 300px; 
     width: 100%; 
     object-fit: cover;
 }
 
-ul {
-    list-style-type: none; 
-    display: flex; 
-    gap: 20px; 
-    font-weight: bold;
+.fa-thumbs-up  {
+    font-size: 25px;
+    color: #4E5166; 
+}
+
+.fa-thumbs-up:active {
+    color: #FD2D01; 
 }
 
 

47de2d6199995f62f2acc3a917445962eacc2e38
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-09-07 15:42:10 +0200

    init

---
+++ frontend/groupomania-frontend/src/views/Profil.vue
@@ -0,0 +1,21 @@
+<template>
+    <user-profil/>
+</template>
+
+<script>
+import UserProfil from '@/components/UserProfil.vue'
+export default({
+    name: "Profil",
+    components: {
+        UserProfil
+    }
+
+
+})
+</script>
+
+<style scoped>
+h1 {
+    color: #fff
+}
+</style>
---
+++ frontend/groupomania-frontend/src/views/UsersPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <user-infos/>
+</template>
+
+<script>
+import UserInfos from '@/components/UserInfos.vue'
+export default({
+    name: "UsersPage", 
+    components: {
+        UserInfos
+    }
+})
+</script>
+
+<style scoped>
+
+</style>

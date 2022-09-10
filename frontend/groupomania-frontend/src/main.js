8cb1e09f55c3cf57587d75edf32693ef56a54875
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-09-10 15:57:26 +0200

    fix

--- frontend/groupomania-frontend/src/main.js
+++ frontend/groupomania-frontend/src/main.js
@@ -7,9 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import axios from 'axios'
 
-
 const token = localStorage.getItem('token')
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
-// axios.defaults.headers.post['Content-type'] = 'application/json'
 
 createApp(App).use(router).mount('#app')
